18b93b26424061df91ad78497fdf4c972a399751
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-20 18:08:57 -0400

    CityScreen: get City Weather method Weather class

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,15 @@ const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    Uri openWeatherUri =
+        Uri.parse('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    NetworkHelper networkHelper = NetworkHelper(openWeatherUri);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
